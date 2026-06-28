4aff196269e7b4c147c83ca533fb9da3b7247737
shadi-1122 <shahinshadi1122@gmail.com>
2026-06-28 16:02:06 +0530

    threshold updated

--- lib/person-service.ts
+++ lib/person-service.ts
@@ -2,7 +2,7 @@ import Person from "@/models/Person";
 
 import { cosineSimilarity } from "./similarity";
 
-const FACE_THRESHOLD = Number(process.env.FACE_MATCH_THRESHOLD) || 0.62;
+const FACE_THRESHOLD = Number(process.env.FACE_MATCH_THRESHOLD) || 0.82;
 
 type BBox = {
   x: number;
